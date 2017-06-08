670405b2f374b8c036246048505912ce53478df1
Nathan Gallup <nathan.gallup@nrel.gov>
2017-06-08 13:07:25 -0600

    Stripped some features.  gaussdas is correctly finding thermochem data.
    Removed a few things for simplicity and implemented a barbaric keyword search scheme to map keywords within the Gaussian output file to a dictionary of parse functions.

--- gaussdas/processor.py
+++ gaussdas/processor.py
@@ -3,9 +3,6 @@ import os.path
 import pandas
 from subroutines import Subroutines
 
-# Might as well utilize a global routines files
-routines = Subroutines()
-
 class Processor(object):
     '''
     The Processor class will act as the only class the user should interact
@@ -15,29 +12,51 @@ class Processor(object):
     '''
     def __init__(self):
         self.indices = []
+        self.routines = Subroutines()
 
-    def get_df(self, file_path):
-        df = None
+    def get_df(self, file_path, df=None):
+        if df == None:
+            df = pandas.DataFrame()
+
         path = os.path.abspath(file_path)
 
-        self.indices = self._get_indices(path)
-        print(self.indices) #DELETE
+        #self.indices = self._get_indices(path)
+        #print(self.indices) #DELETE
+        df_copy = df.copy()
+        new_df = self._search_and_parse(path, df_copy)
         
-        return df
+        return new_df 
                 
     def _get_indices(self, path):
         '''
-        There are probably some ways to optimize this to prevent reading the
-        file twice, but this should work for now
+        For a scheme involving reading the file twice.  First pass to obtain
+        the data token indices, or potential indices.  A followup pass would
+        parse out the relevant data at those indices.
         '''
         indices = []
         with open(path) as infile:
             for i, line in enumerate(infile):
-                char_inds, func_call = routines.find_token_indices(line)
+                print('LINE: ', line) #DELETE
+                char_inds, func_call = self.routines.find_token_indices(line)
                 for cind in char_inds:
                     cind = [i, cind, func_call]
 
                 for each in char_inds:
                     indices.append(each)
 
-        return indices
+        return indices  
+
+    def _search_and_parse(self, path, df):
+        '''
+        Perform a linear scanning of the output file to determine if keywords
+        exist within the current line.  If so, perform the appropriate
+        subroutine, reading in the necessary amount text, and update the
+        pandas dataframe and begin scanning from a later point in the file
+        stream
+        '''
+        with open(path) as infile:
+            for line in infile:
+                #print(line, infile, self.routines.brute_search) #DELETE
+                infile, df = self.routines.brute_search(infile, line, df)
+        
+        return df
--- gaussdas/subroutines.py
+++ gaussdas/subroutines.py
@@ -1,21 +1,48 @@
 import pandas
 
-class Decision_Tree(object):
-    pass
-
 class Subroutines(object):
     def __init__(self):
         self._keys = {}
         self._keys['Zero-point correction'] = thermo_chem
         
-
-
     def find_token_indices(self, line):
-        for char in line:
-            pass
+        #for char in line:
+        #    pass
+        pass
+
+    def brute_search(self, infile, line, df):
+        '''
+        This is a fairly rudimentary algorithm just to get gaussdas up and
+        running.  It is extremely inefficient at finding the keywords and
+        the subsequent parsing schemes and needs to be replaced with an
+        efficient algorithm for multiple keyword matching.  There are
+        efficient schemes that exist for this but are fairly sophisticated.
+        '''
+        for keyword in self._keys:
+            if keyword in line:
+                infile, df = self._exec_parse(keyword, line, infile, df)
+        
+        return infile, df 
+
+    def _exec_parse(self, key, line, infile, df):
+        '''
+        For executing a function that maps to a keyword and appending the
+        resulting data to the dataframe before handing back the dataframe
+        and file stream -- or performing some other function
+        '''
+        func_call = self._keys[key]
+        infile, new_df = func_call(infile, line, df)
+
+        # Check if function is void type or making changes to the DataFrame
+        if new_df != None:
+            df = new_df
+
+        return infile, df
             
-def thermo_chem(filestream):
+def thermo_chem(filestream, line, df):
     '''
     Gather all thermochemistry data in the event of successful freq
     '''
+    print(line)
+    print("I'm the thermo_chem function")
     pass
